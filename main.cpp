26b49f31c9673635b700a0c9dd2f1d4592bad8b0
Chris VZ <chris.vonzuben@gmail.com>
2014-11-11 20:37:36 -0500

    added function to surpress system call overhead for tests

--- main.cpp
+++ main.cpp
@@ -5,19 +5,32 @@
 #include<algorithm>
 #include<cstdlib>
 
-#define MEMSIZE 4096/4
+#define MEMSIZE 16
+#define NUMTESTS 5
 
 template <typename T, typename... args>
-void timeit(std::string testname, T f, args&&... a){
+void timeit(std::string testname, int numt, T f, args&&... a){
 
-    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    for (int i = 0; i < numt; i++){
+        std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
-    f(std::forward<args>(a)...);
+        f(std::forward<args>(a)...);
 
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << testname << ": Time "
-              << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
-              << "ns.\n";
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        std::cout << testname << " " << i + 1 << " " << ": Time "
+                  << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
+                  << "ns.\n";
+    }
+}
+
+// this just grabs memory from the stack and heap to perform the system call and then returns it to the program
+// this way the tests are not affected by system call overhead
+void grabMem(){ 
+
+    double tmp[MEMSIZE];
+    double *t = new double[MEMSIZE];
+
+    delete[]t;
 }
 
 int main(int argc, char **argv){
@@ -25,9 +38,11 @@ int main(int argc, char **argv){
     std::cout << "MEMSIZE (num double): " << MEMSIZE << '\n';
     std::cout << "size of double: " << sizeof(double) << '\n';
 
+    grabMem();
+
     double hello; // this is not for testing performance, but testing template stuff
 
-    timeit("Heap alloc 1", [](double *hello){
+    timeit("Heap alloc", NUMTESTS, [](double *hello){
 
             double *p = new double[MEMSIZE];
 
@@ -37,32 +52,15 @@ int main(int argc, char **argv){
 
     }, &hello);
 
-    // this test shows that the first malloc for the data section is the most expensive because of the system call,
-    // but after freeing it, if we need the memory again than it is alrady held by the internal new/delete memory allocator
-    // and is obtainable with out system call. for both heap and stack memory cases
-
-    timeit("Heap alloc 2", [](){ 
-
-            double *p = new double[MEMSIZE];
-
-            delete[]p;
-
-    });
-
-
     double buf[MEMSIZE];
 
     srand(3);
 
-    for (auto &x : buf){ // this isnt working p is all 0's evan after loop
+    for (auto &x : buf){
         x = static_cast<double>(rand())/static_cast<double>(RAND_MAX);
     }
 
-    // big problem, all these tests are affected by the first one. everything after goes faster even after changeing the
-    // content of buf befor each copy, I think it is because stack memory is malloc'ed for the first one and that memory is still held
-    // by the program, allowing copying to be faster.
-    
-    timeit("memmove", [&](){
+    timeit("memmove", NUMTESTS, [&](){
 
             double d[MEMSIZE];
 
@@ -70,30 +68,21 @@ int main(int argc, char **argv){
 
     });
 
-    for (auto &x : buf){ // this isnt working p is all 0's evan after loop
-        x = static_cast<double>(rand())/static_cast<double>(RAND_MAX);
-    }
-
-    timeit("memcopy", [&](){
+    timeit("memcopy", NUMTESTS, [&](){
 
             double d[MEMSIZE];
 
             memcpy(d, buf, MEMSIZE*sizeof(double));
 
     });
-    
-    for (auto &x : buf){ // this isnt working p is all 0's evan after loop
-        x = static_cast<double>(rand())/static_cast<double>(RAND_MAX);
-    }
 
-    timeit("std::copy", [&](){
+    timeit("std::copy", NUMTESTS, [&](){
 
             double d[MEMSIZE];
 
             std::copy(buf, &buf[MEMSIZE], d);
 
     });
-
 
     std::cout << hello << '\n';
     return 0;
